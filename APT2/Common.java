0b46ada1f3c05c790ccb7e2f6db571d1098f6d2e
Chris Hill <chris.hill@duke.edu>
2026-09-06 16:21:21 -0400

    finished all apt2s

---
+++ APT2/Common.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+public class Common {
+     public int count (String a, String b) 
+     {
+        int count = 0;
+        ArrayList<String> repeats = new ArrayList<>();
+
+        for(int i = 0; i <a.length(); i ++)
+            {
+                String x = a.substring(i,i+1);
+                int num = b.indexOf(x);
+
+                if(num != -1) 
+                {
+                    count++;
+                    b = b.substring(0,num)+ b.substring(num+1);
+                    
+                }
+                
+            
+            }
+
+        return count;
+     }
+  }
